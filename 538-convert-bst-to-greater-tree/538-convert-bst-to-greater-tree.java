85954b9e39321eb8faf866e7adc72786d6371330
Prashant Mishra <pm7044872@gmail.com>
2022-06-15 15:18:20 +0530

    Time: 469 ms (5.73%), Space: 51.5 MB (5.82%) - LeetHub

--- 538-convert-bst-to-greater-tree/538-convert-bst-to-greater-tree.java
+++ 538-convert-bst-to-greater-tree/538-convert-bst-to-greater-tree.java
@@ -19,12 +19,22 @@ class Solution {
     public TreeNode convertBST(TreeNode root) {
         //we know that inorder traversal of the bst will result in sorted array;
         inorder(root);
+        
+        /*
+        now lets take an example : 
+        inorder of [4,1,6,0,2,5,7,null,null,null,3,null,null,null,8] 
+        will be : 0,1,2,3,4,5,6,7,8
+        now for 0 there are 8 values that are greater that it so 0 will become : 0+1+2+3+4+5+6+7+8 = 36
+        similarly 1 will become : 36 and so on 
+finally greater result  : [36, 36, 35, 33, 30, 26, 21, 15, 8]
+Thats what below code calculates
+        */
         for( int i =0;i< list.size();i++){
-            temp.add(giveSum(list,i,list.size()));
+            temp.add(giveSum(list,i,list.size())); 
         }
-        System.out.println(list);
-        System.out.println(temp);
+        // finally again do the inorder traversal and update the old value with new ones
         recreatingTree(root);
+        // root will have updated values now 
         return root;
     }
     public void inorder(TreeNode root){
