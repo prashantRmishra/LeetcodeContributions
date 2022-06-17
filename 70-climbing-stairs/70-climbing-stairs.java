6030e1f4ceccc1a99004e1dcd201414de72a833f
Prashant Mishra <pm7044872@gmail.com>
2022-06-17 21:46:19 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (11.17%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,8 +1,9 @@
 class Solution {
-    int dp[];
+   
+   /*
+   
     public int climbStairs(int n) {
-      
-        dp = new int[n+1];
+        int dp = new int[n+1];
         Arrays.fill(dp,-1);
         dp[0]  = 1;
         dp[1] = 1;
@@ -10,16 +11,28 @@ class Solution {
             dp[i] = dp[i-1]+dp[i-2];
         }
         return dp[n];
+    }*/
+    
+    public int climbStairs(int n){
+        int prev  = 1;
+        int prev2 = 1;
+        for(int i =2;i<=n;i++){
+            int curr_i = prev+prev2;
+            prev2 = prev;
+            prev = curr_i;
+        }
+        return prev;
+        
     }
     
-    public int isPossible(int n){
-        if(dp[n]!=-1) return dp[n];
-        if(n == 0) {
+//     public int isPossible(int n){
+//         if(dp[n]!=-1) return dp[n];
+//         if(n == 0) {
             
-            return 1;
-        }
-        if(n ==1 ) return 1;
+//             return 1;
+//         }
+//         if(n ==1 ) return 1;
      
-        return isPossible(n-1) + isPossible(n-2);
-    }
+//         return isPossible(n-1) + isPossible(n-2);
+//     }
 }
