3a3f32e75fa71fe001acd1ac8e7b3b05abda79ce
Prashant Mishra <pm7044872@gmail.com>
2022-06-19 19:56:13 +0530

    Time: 17 ms (5.46%), Space: 44.3 MB (38.58%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.java
+++ 90-subsets-ii/90-subsets-ii.java
@@ -1,3 +1,8 @@
+
+// approach one : using recursion : though this solutions works but,
+ // if we think of tc its o(2^n)k^2.logk // as o(2^n) for recursion , k : if we assume average length of each subset is k , then k subsets are put in another List i.e. 'list', and klogk for sorting list of size k 
+// moral of the story is we will have to optimize this solution to get a better tc
+/*
 class Solution {
     List<List<Integer>> list = new ArrayList<>();
     public List<List<Integer>> subsetsWithDup(int[] nums) {
@@ -12,6 +17,34 @@ class Solution {
         if(i>=nums.length) {
             List<Integer> temp = new ArrayList<>(l);
             Collections.sort(temp);
+            if(!list.contains(temp))
+                list.add(temp);
+            return;
+        }
+        l.add(nums[i]);
+        findSubsets(i+1,nums,l);
+        l.remove(l.size()-1);
+        findSubsets(i+1,nums,l);
+    }
+}
+**/
+
+// solution 2 
+// optimal sol :tc o(2^n)*k
+class Solution {
+    List<List<Integer>> list = new ArrayList<>();
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        // for getting all the possible subsets we can use recursion the 
+        // way we used for subset I 
+        // but we will have to avoid having duplicates 
+        Arrays.sort(nums);
+        findSubsets(0,nums,new ArrayList<>());
+        return list;
+        
+    }
+    public void findSubsets(int i, int[] nums, List<Integer> l){
+        if(i>=nums.length) {
+            List<Integer> temp = new ArrayList<>(l);
             if(!list.contains(temp))
                 list.add(temp);
             return;
