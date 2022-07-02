92c490260a5f63709330a3b9a81c03e27883d8c0
Prashant Mishra <pm7044872@gmail.com>
2022-07-02 18:37:52 +0530

    Added solution - LeetHub

--- Max Sum without Adjacents - GFG/max-sum-without-adjacents.java
+++ Max Sum without Adjacents - GFG/max-sum-without-adjacents.java
@@ -30,21 +30,40 @@ public class Main {
 //User function Template for Java
 
 class Solution {
-    int findMaxSum(int arr[], int n) {
-        // code here
+    // memoization top down  approach tc  : O(n) and space is O(n) space we used for creating dp array
+    // int findMaxSum(int arr[], int n) {
+    //     // code here
         
-        // we are starting in reverse Order
+    //     // we are starting in reverse Order
+    //     int dp[] = new int[n];
+    //     Arrays.fill(dp,-1);
+    //     return solve(arr,n-1,dp);
+    // }
+    // public int solve(int arr[], int index,int dp[]){
+       
+    //     if(index==0) return arr[0];
+    //     if(index<0) return 0;
+    //     if(dp[index]!=-1) return dp[index];
+    //     int takeIt = arr[index] + solve(arr,index-2,dp);// as we have taken i we can't take i-1, we have to take i-2
+    //     int donttakeIt = 0+solve(arr,index-1,dp); // as we didnt take the i the element we can take the i-1 th element.
+    //     return dp[index] = Integer.max(takeIt,donttakeIt);
+    // }
+    // bottom up approach ie tabulation tc and space complexity is same
+    int findMaxSum(int arr[], int n){
         int dp[] = new int[n];
         Arrays.fill(dp,-1);
-        return solve(arr,n-1,dp);
+        dp[0] = arr[0];
+        int negativeIndexVal = 0;
+        for(int i =1;i<n;i++){
+            int take  = arr[i]; 
+            if(i>1) take+=dp[i-2];
+            int dontTake = 0 + dp[i-1];
+            
+            dp[i] = Math.max(take,dontTake);
+        }
+        return dp[n-1];
+        
     }
-    public int solve(int arr[], int index,int dp[]){
-       
-        if(index==0) return arr[0];
-        if(index<0) return 0;
-        if(dp[index]!=-1) return dp[index];
-        int takeIt = arr[index] + solve(arr,index-2,dp);// as we have taken i we can't take i-1, we have to take i-2
-        int donttakeIt = 0+solve(arr,index-1,dp); // as we didnt take the i the element we can take the i-1 th element.
-        return dp[index] = Integer.max(takeIt,donttakeIt);
-    }
+    
+    //t
 }
