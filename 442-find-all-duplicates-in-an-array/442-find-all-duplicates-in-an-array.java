07a1ed599c53ee115da02ff759dbd9887981aba4
Prashant Mishra <pm7044872@gmail.com>
2022-07-07 22:06:00 +0530

    Time: 8 ms (77.34%), Space: 66.3 MB (56.97%) - LeetHub

--- 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.java
+++ 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.java
@@ -1,12 +1,35 @@
+// class Solution {
+//     public List<Integer> findDuplicates(int[] nums) {
+//         List<Integer> result =new ArrayList<>();
+//         Set<Integer> set = new HashSet<>();
+//         for(int i  : nums){
+//             if(!set.add(i)){
+//                 result.add(i);
+//             }
+//         }
+//         return result;
+//     }
+// }
+//the above approach is useless though it works, as it is not using space of O(1)
 class Solution {
-    public List<Integer> findDuplicates(int[] nums) {
-        List<Integer> result =new ArrayList<>();
-        Set<Integer> set = new HashSet<>();
-        for(int i  : nums){
-            if(!set.add(i)){
-                result.add(i);
+    public List<Integer> findDuplicates(int[] nums){
+        List<Integer> list = new ArrayList<>();
+        int i =0,n  = nums.length;
+        while(i<n){
+            int j = nums[i]-1;
+            if(nums[i]!=nums[j]){
+                swap(nums,i,j);
             }
+            else i++;
         }
-        return result;
+        for(int j =0;j<n;j++){
+            if(nums[j]!=j+1) list.add(nums[j]);
+        }
+        return list;
+    }
+    public void swap(int[] nums,int i ,int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
