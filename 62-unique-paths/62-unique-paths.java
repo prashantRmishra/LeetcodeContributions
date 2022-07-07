5a6925e936d6a274aa35c9654c74d7df4bb6a889
Prashant Mishra <pm7044872@gmail.com>
2022-07-07 15:48:01 +0530

    Time: 2 ms (7.85%), Space: 40.8 MB (63.98%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -6,6 +6,32 @@ class Solution {
         for(int[] row : dp){
             Arrays.fill(row,-1);
         }
+      for(int i =0;i<grid.length;i++){
+          for(int j =0;j<grid[0].length;j++){
+              int left = 0, up = 0;
+              if(i== 0 && j ==0) dp[0][0] =1;
+              else {
+                  if(j>0) left = dp[i][j-1];
+                  if(i>0) up = dp[i-1][j];
+                  dp[i][j] = left + up;
+              }
+          }
+          
+      }
+        return dp[m-1][n-1];
+    }
+}
+
+/*
+// Memoization solution
+class Solution {
+    int count  = 0;
+    public int uniquePaths(int m, int n) {
+        int[][] grid = new int [m][n];
+        int[][] dp = new int[m][n];
+        for(int[] row : dp){
+            Arrays.fill(row,-1);
+        }
         return solve(grid,0,0,dp);
         
     }
@@ -24,4 +50,4 @@ class Solution {
         int right = solve(grid,i,j+1,dp);
         return dp[i][j] = left+right;
     }
-}
+}*/
