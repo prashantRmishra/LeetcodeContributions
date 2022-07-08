3410f626c9bda6bc4fe310bff07ebfd255d3cbca
Prashant Mishra <pm7044872@gmail.com>
2022-07-08 12:26:13 +0530

    Time: 1 ms (99.97%), Space: 45.2 MB (81.12%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.java
+++ 64-minimum-path-sum/64-minimum-path-sum.java
@@ -5,20 +5,20 @@ class Solution {
         for(int row[] : dp){
             Arrays.fill(row,-1);
         }
-    return solve(grid,grid.length-1,grid[0].length-1,dp);
+    return solve(grid,0,0,dp);
         
     }
     public int solve(int[][] g, int i , int j,int dp[][]){
-        if(i<0 || j<0) return Integer.MAX_VALUE;
+        if(i>g.length-1 || j>g[0].length-1) return Integer.MAX_VALUE;
         if(dp[i][j]!=-1) return dp[i][j];
-        if(i==0 && j == 0){
+        if(i==g.length-1 && j == g[0].length-1){
             return g[i][j];
         }
         
         
-        int up  = solve(g,i-1,j,dp);
-        int left = solve(g,i,j-1,dp);
-        return dp[i][j] = g[i][j]+Integer.min(up,left);
+        int  down = solve(g,i+1,j,dp);
+        int right = solve(g,i,j+1,dp);
+        return dp[i][j] = g[i][j]+Integer.min(down,right);
         
     }
 }
