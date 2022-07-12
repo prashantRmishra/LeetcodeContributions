c5f06ad17812dd0a14ce9f77732d3812b500c791
Prashant Mishra <pm7044872@gmail.com>
2022-07-12 21:11:50 +0530

    Time: 17 ms (34.13%), Space: 46.1 MB (15.55%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.java
+++ 9-palindrome-number/9-palindrome-number.java
@@ -2,11 +2,11 @@ class Solution {
     public boolean isPalindrome(int x) {
         StringBuilder br = new StringBuilder();
         br.append(x); 
-        String str =  br.toString();
-        return isPali(str);
-        // String str  = br.reverse().toString();
-        // if(str.equals(String.valueOf(x))) return true;
-        // return false;
+       
+        //return isPali(str);
+        String str  = br.reverse().toString();
+        if(str.equals(String.valueOf(x))) return true;
+        return false;
     }
     public boolean isPali(String str){
         int i =0;
