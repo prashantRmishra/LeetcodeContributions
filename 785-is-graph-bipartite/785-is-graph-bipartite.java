4ad1fb498c761524ada02c8c684369213dfcf327
Prashant Mishra <pm7044872@gmail.com>
2022-07-23 12:42:02 +0530

    Time: 1 ms (88.90%), Space: 53.6 MB (56.83%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.java
+++ 785-is-graph-bipartite/785-is-graph-bipartite.java
@@ -1,36 +1,34 @@
 class Solution {
     public boolean isBipartite(int[][] graph) {
-        ArrayList<ArrayList<Integer>> list =new ArrayList<>();
-        for(int i =0;i<graph.length;i++){
-            list.add(new ArrayList<>());
-        }
-        
-        for(int  i =0;i<graph.length;i++){
-            for(int j =0;j<graph[i].length;j++){
-                ArrayList<Integer> l = list.get(i);
-                l.add(graph[i][j]);
-                list.set(i,l);
-            }
-           
-        }
+        // ArrayList<ArrayList<Integer>> list =new ArrayList<>();
+        // for(int i =0;i<graph.length;i++){
+        //     list.add(new ArrayList<>());
+        // }
+        // for(int  i =0;i<graph.length;i++){
+        //     for(int j =0;j<graph[i].length;j++){
+        //         ArrayList<Integer> l = list.get(i);
+        //         l.add(graph[i][j]);
+        //         list.set(i,l);
+        //     }
+        //}
         int visited[] = new int[graph.length];
         int color[] = new int[graph.length];
         for(int i =0;i<graph.length;i++){
             if(visited[i]==0){
                 visited[i] =1;
                 color[i] =0;
-                if(!isPossible(i,visited,color,list)) return false;
+                if(!isPossible(i,visited,color,graph)) return false;
             }
         }
         return true;
         
     }
-    public boolean isPossible(int node, int[] visited, int color[], ArrayList<ArrayList<Integer>> list){
+    public boolean isPossible(int node, int[] visited, int color[], int graph[][]){
         Queue<Integer> q = new LinkedList<>();
         q.add(node);
         while(!q.isEmpty()){
             Integer n = q.remove();
-            for(Integer it: list.get(n)){
+            for(int it: graph[n]){
                 if(visited[it]==0){
                     visited[it] = 1;
                     if(color[n]==0) color[it] = 1;
@@ -38,7 +36,6 @@ class Solution {
                     q.add(it);
                 }
                 else if(color[it]==color[n]){
-                    //System.out.println(it +" and "+ n+" have same color");
                      return false;
                 }
             }
