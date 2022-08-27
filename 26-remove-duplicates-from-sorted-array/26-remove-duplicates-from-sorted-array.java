2fb0633c1c4e7e0fcd4372f563c8135f1902836b
Prashant Mishra <pm7044872@gmail.com>
2022-08-27 17:08:30 +0530

    Time: 2 ms (42.54%), Space: 48 MB (40.58%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.java
@@ -1,16 +1,37 @@
-class Solution {
-    public int removeDuplicates(int[] nums) {
+
+//TC: o(N) + o(N) for two passes,space complexity : o(N) for using list
+//not optimal
+// class Solution {
+//     public int removeDuplicates(int[] nums) {
       
-        List<Integer> list = new ArrayList<>();
-        int count = 0;
-        for(int i : nums)
-            if(!list.contains(i))
-               list.add(i);
-        int index = 0;
-        for(Integer val: list){
-            nums[index] = val;
-            index++;
+//         List<Integer> list = new ArrayList<>();
+//         int count = 0;
+//         for(int i : nums)
+//             if(!list.contains(i))
+//                list.add(i);
+//         int index = 0;
+//         for(Integer val: list){
+//             nums[index] = val;
+//             index++;
+//         }
+//         return list.size();
+//     }
+// }
+class Solution {
+    public int removeDuplicates(int[] nums){
+        int i =0,j = i+1;
+        while(j<nums.length){
+            if(nums[i] == nums[j]){
+                j++;
+                continue;
+            }
+            if(nums[i] !=nums[j]){
+                int temp = nums[j];
+                nums[i+1] = nums[j];
+                nums[j] = temp;
+                i++;
+            }
         }
-        return list.size();
+        return i+1;
     }
 }
