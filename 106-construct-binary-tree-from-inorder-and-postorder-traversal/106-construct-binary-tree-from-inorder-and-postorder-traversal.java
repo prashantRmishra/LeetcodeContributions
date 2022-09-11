8d52c12f3b9a966bb759a3d577ef2d57b3a29cf1
Prashant Mishra <pm7044872@gmail.com>
2022-09-11 20:26:42 +0530

    Time: 2 ms (98.51%), Space: 42.2 MB (90.96%) - LeetHub

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/106-construct-binary-tree-from-inorder-and-postorder-traversal.java
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/106-construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -15,18 +15,18 @@
  */
 class Solution {
     public TreeNode buildTree(int[] inorder, int[] postorder) {
-        List<Integer> in = new ArrayList<>();
-        List<Integer> po = new ArrayList<>();
-        for(int i: inorder) in.add(i);
-        for(int i: postorder) po.add(i);
-        return generateTree(0,in.size()-1,0,po.size()-1,in,po);
+        Map<Integer,Integer> in = new HashMap<>();
+        
+        for(int i =0;i<inorder.length;i++) in.put(inorder[i],i);
+       
+        return generateTree(0,in.size()-1,0,postorder.length-1,in,postorder);
         
     }
-    public TreeNode generateTree(int inmin, int inmax,int pomin,int pomax,List<Integer> in, List<Integer> po){
+    public TreeNode generateTree(int inmin, int inmax,int pomin,int pomax,Map<Integer,Integer> in, int[] po){
         if(inmin > inmax || pomin > pomax ) return null;
-        int val = po.get(pomax);//last element of postorder is the root element
+        int val = po[pomax];//last element of postorder is the root element
         TreeNode root = new TreeNode(val);
-        int rIndex = in.indexOf(val);
+        int rIndex = in.get(val);
         root.left = generateTree(inmin,rIndex-1,pomin,pomin+rIndex-inmin-1,in,po);
         root.right = generateTree(rIndex+1,inmax,pomin+rIndex-inmin,pomax-1,in,po);
         return root;
