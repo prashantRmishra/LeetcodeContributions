2581aeb532e3d4004c8b250c6216896b7ad3df84
Prashant Mishra <pm7044872@gmail.com>
2022-09-14 00:16:59 +0530

    Time: 297 ms (8.36%), Space: 105.8 MB (10.24%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
@@ -1,47 +1,26 @@
 class Solution {
-    
-    // brute force approach with o(n^2) time compexity 
-   /**
     public int lengthOfLongestSubstring(String s) {
-        if(s.length() ==0) return 0;
-        List<Character> list   = new ArrayList<>();
-        int size = Integer.MIN_VALUE;
-        for(int j =0;j< s.length();j++){  
-            for(int i =j;i< s.length();i++){
-                if(!list.contains(s.charAt(i))){
-                    list.add(s.charAt(i));
-                }
-                else {
-                     size = size > list.size() ? size : list.size();
-                     list.clear();
-                    break;
-                }
+        if(s.equals(null) || s.equals("")) return 0;
+        
+		HashSet<Character> set = new HashSet<>();
+		int len = 0;
+		int max =0;
+		int i =0,j=0;
+     
+        while(j<s.length()){
+            if(!set.contains(s.charAt(j))){
+                set.add(s.charAt(j));
+                j++;
+                len++; 
+                max  = Integer.max(len,max);
             }
-         }
-        size = size > list.size() ? size : list.size();
-        return size;
-    }
-    
-   */
-    //two pointer solution 
-       public int lengthOfLongestSubstring(String s) {
-           if(s.length() ==0) return 0;
-           List<Character> list   = new ArrayList<>();
-           int size = Integer.MIN_VALUE;
-           int i = 0;int j =i;
-           while(i<s.length() && j< s.length() && i<=j){
-               if(!list.contains(s.charAt(j))){
-                   list.add(s.charAt(j));
-                   j++;
-               }
-               else {
-                   size = size > list.size() ? size : list.size();
-                   list.clear();
-                   i++;
-                   j = i;
-               }
-           }
-           size = size > list.size() ? size : list.size();
-           return size;
+            else{
+                len =0;
+                set.clear();
+                j = i+1;
+                i++;
+            }
+        }
+        return max;
     }
 }
